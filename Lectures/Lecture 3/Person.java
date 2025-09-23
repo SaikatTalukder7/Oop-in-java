a31d1a5545b2e6d3a38602c323fcd164bdc98f06
Saikat Talukder <saikat60mu@gmail.com>
2025-09-24 00:38:01 +0600

    Create Person.java

---
+++ Lectures/Lecture 3/Person.java
@@ -0,0 +1,13 @@
+package Lecture3;
+
+public class Person {
+	
+	String Name1; 
+	int Age;
+
+	public void information() {
+
+		System.out.println("Person Name: "+ Name1 +"\n"+ "Person Age: "+ Age);
+	}
+
+}
