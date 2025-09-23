e367014645050d389e4565d2f3554c26d26e2abe
Saikat Talukder <saikat60mu@gmail.com>
2025-09-24 00:35:39 +0600

    Create Main2.java

---
+++ Lectures/Lecture 1/Main2.java
@@ -0,0 +1,20 @@
+package Lecture1;
+
+public class Main2 {
+
+	public static void main(String[] args) {
+
+		int a1 = 10;
+		int b1 = 5;
+		int sum1 = a1 + b1;
+		System.out.println("Sum: "+sum1);
+		
+		
+		double a2 = 10.241115051;
+		double b2 = 5.241115051 ;
+		double sum2 = a2 + b2;
+		System.out.println("Sum: "+sum2);
+
+	}
+
+}
