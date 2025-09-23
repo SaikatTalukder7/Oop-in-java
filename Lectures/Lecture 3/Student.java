21eaf7c6baa5c282c24d1442d92a212b15de2dbd
Saikat Talukder <saikat60mu@gmail.com>
2025-09-24 00:38:23 +0600

    Create Student.java

---
+++ Lectures/Lecture 3/Student.java
@@ -0,0 +1,14 @@
+package Lecture3;
+
+public class Student {
+	
+	String Name2;
+	int id;
+
+	void details() {
+		
+		System.out.println("The name of the student "+Name2+" and the id is "+id);
+
+	}
+
+}
