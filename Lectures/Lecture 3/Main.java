67c25e8b47d2e2c592ea83e19b312f5ca971a987
Saikat Talukder <saikat60mu@gmail.com>
2025-09-24 00:37:37 +0600

    Create Main.java

---
+++ Lectures/Lecture 3/Main.java
@@ -0,0 +1,22 @@
+package Lecture3;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		Person obj1 = new Person();
+		obj1.Name1 = "Saikat";
+		obj1.Age = 21;
+		
+		obj1.information();
+		
+		
+		Student obj2 = new Student();
+		obj2.Name2 = "Saikat Talukder";
+		obj2.id = 241115051;
+		
+		obj2.details();
+
+	}
+
+}
