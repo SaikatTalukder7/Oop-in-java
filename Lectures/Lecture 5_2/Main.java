e4a2cc037ff043c768066f650a51b6bb172043a3
Saikat Talukder <saikat60mu@gmail.com>
2025-10-07 19:12:24 +0600

    Create Main.java

---
+++ Lectures/Lecture 5_2/Main.java
@@ -0,0 +1,16 @@
+package Lecture5_2;
+
+import Lecture5_1.Book;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		Car c1 = new Car("Life Of SKT7",127.8F,2003);
+		c1.carDetails();
+		
+		Car c2 = new Car("Life Of SKT7",127.8F,2003,"Grey");
+		c2.carDetails();
+	}
+
+}
