43706fd89f8de776257368fa859745f8d02093b9
Saikat Talukder <saikat60mu@gmail.com>
2025-10-07 19:08:08 +0600

    Create Main.java

---
+++ Lectures/Lecture 5_1/Main.java
@@ -0,0 +1,15 @@
+package Lecture5_1;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Book b1 = new Book("Life Of SKT7","Saikat Talukder", 500);
+		b1.shelf();
+		
+		Book b2 = new Book("Life Of SKT7","Saikat Talukder", 500,"Grey");
+		b2.shelf();
+
+	}
+
+}
