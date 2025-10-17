a7f3fc789375486702779cb79102fbbf2d59db7d
Saikat Talukder <saikat60mu@gmail.com>
2025-10-17 23:32:49 +0600

    Add files via upload

---
+++ Lectures/Lecture 6_1/Pet.java
@@ -0,0 +1,12 @@
+package Lecture6_1;
+
+public class Pet {
+
+	String pet_name;
+	int age;
+	void displayinfo(String pet_name, int age) 
+	{
+		System.out.println("Pet Name:"+ pet_name + "\nAge:"+ age);
+	}
+	
+}
