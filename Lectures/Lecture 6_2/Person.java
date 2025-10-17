40273c9a24d81ace391dded9546e4bbbaa07c756
Saikat Talukder <saikat60mu@gmail.com>
2025-10-17 23:37:39 +0600

    Add files via upload

---
+++ Lectures/Lecture 6_2/Person.java
@@ -0,0 +1,12 @@
+package Lecture6_2;
+
+public class Person {
+
+	String person_name;
+	int id;
+	void showinfo(String person_name, int id)
+	{
+		System.out.println("Name:" + person_name + "\nID:" + id);
+	}
+
+}
