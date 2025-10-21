7dcdf49ebbff58cffb7e98b6b5bf7488f6bb5db1
Saikat Talukder <saikat60mu@gmail.com>
2025-10-21 21:43:48 +0600

    Create Manager.java

---
+++ Lectures/Lecture7_1/Manager.java
@@ -0,0 +1,14 @@
+package Lecture7_1;
+
+public class Manager extends Employee {
+
+	void work()
+	{
+		System.out.println("Manager managing");
+	}
+	void manageTeam()
+	{
+		System.out.println("Managing team");
+	}
+
+}
