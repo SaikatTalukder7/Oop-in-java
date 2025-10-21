95ad7bd9b2496a8a3d13f25e58197651a6556860
Saikat Talukder <saikat60mu@gmail.com>
2025-10-21 21:51:57 +0600

    Create Dog.java

---
+++ Lectures/Lecture7_2/Dog.java
@@ -0,0 +1,14 @@
+package Lecture7_2;
+
+public class Dog extends Animal {
+
+	void makeSound()
+	{
+		System.out.println("Dog barking");
+	}
+	void fetch()
+	{
+		System.out.println("Fetching ball");
+	}
+
+}
